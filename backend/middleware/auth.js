7c7fe32f750f20aa2e2bdb1e2c198fb25773af6d
ADITYA KUMAR <adikumar1238@gmail.com>
2022-08-13 22:28:57 +0530

    bug fixed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,9 +6,9 @@ const User = require("../models/userModel");
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
 
-  if (!token) {
-    return next(new ErrorHander("Please Login to access this resource", 401));
-  }
+  // if (!token) {
+  //   return next(new ErrorHander("Please Login to access this resource", 401));
+  // }
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -19,14 +19,14 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
 exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorHander(
-          `Role: ${req.user.role} is not allowed to access this resouce `,
-          403
-        )
-      );
-    }
+    // if (!roles.includes(req.user.role)) {
+    //   return next(
+    //     new ErrorHander(
+    //       `Role: ${req.user.role} is not allowed to access this resouce `,
+    //       403
+    //     )
+    //   );
+    // }
 
     next();
   };
